34d8229d892716bf2d9f554758d0fb3f20a9a985
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-05 16:51:41 +0800

    feat: prompt for location and logs weather data API response

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world!");
+let userLocation = prompt("enter a location").replace(/[^a-zA-Z0-9]/g, "%20");
+
+// API units options include 'us', 'metric', uk, 'base
+let units = "us";
+
+// API key (free API)
+const API_KEY = "VSGF7NCUVGWS7PTKPWJPVEKDB";
+
+// Fetch weather data using Weather API
+// Promises syntax
+const weatherPromise = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?key=${API_KEY}`,{
+                            mode: 'cors'
+                            })
+                            .then( (response) => response.json())
+                            .then( (data) => console.log(data))
+                            .catch( (error) => {console.error(error)});
