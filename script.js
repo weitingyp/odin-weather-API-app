f0eff8cc6d3a5c7dcdaca4e4ec065ec580fbf865
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-05 17:00:45 +0800

    refactor: refactor weather data API request to aync/await syntax

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ const API_KEY = "VSGF7NCUVGWS7PTKPWJPVEKDB";
 
 // Fetch weather data using Weather API
 // Promises syntax
+
+/* Commented out to practice async/await syntax
 const weatherPromise = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?key=${API_KEY}`,{
                             mode: 'cors'
                             })
                             .then( (response) => response.json())
                             .then( (data) => console.log(data))
-                            .catch( (error) => {console.error(error)});
+                            .catch( (error) => {console.error(error)}); 
+*/
+
+async function fetchWeatherData(){
+    const weatherResponse = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userLocation}?key=${API_KEY}`,{
+        mode: 'cors'
+        });
+    const dataJson = await weatherResponse.json();
+    console.log(dataJson);
+}
+
+fetchWeatherData().catch(
+    (error) => console.error(error)
+);
